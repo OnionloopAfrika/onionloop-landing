098213330fe252161ddf78e5bd3d3575060565ed
Ononobi Praise <ononobip@gmail.com>
2026-02-11 15:58:36 +0100

    implemented main component design

--- app/config/modes.ts
+++ app/config/modes.ts
@@ -2,9 +2,9 @@ import type { SectionKey } from "./sections";
 
 export const modes = {
   personal: {
-    sections: ["hero", "main", "section", "cta", "accordion"] as SectionKey[],
+    sections: ["hero", "main","trust", "section", "cta", "accordion"] as SectionKey[],
   },
   business: {
-    sections: ["hero", "main", "section", "services", "cta"] as SectionKey[],
+    sections: ["hero", "main","trust", "section", "services", "cta"] as SectionKey[],
   },
 } as const;
--- app/config/sections.ts
+++ app/config/sections.ts
@@ -4,6 +4,7 @@ import Section from "../components/sections/Section";
 import Main from "../components/sections/Main";
 import CTA from "../components/sections/CTA";
 import Accordion from "../components/sections/Accordion"
+import TrustSection from "../components/sections/TrustSection";
 
 
 export const sectionRegistry = {
@@ -13,6 +14,7 @@ export const sectionRegistry = {
   cta: CTA,
   services: Services,
   accordion: Accordion,
+  trust: TrustSection,
 } as const;
 
 export type SectionKey = keyof typeof sectionRegistry;
